ffd8f198c3e819d4629b4896c82a968722a59704
Dillesh Kumar <dellishkumar99@gmail.com>
2018-04-24 18:13:43 +0530

    Add files via upload

---
+++ DateClient.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class DateClient
+{
+public static void main(String args[])
+{
+	try
+ {
+ Socket myClient= new Socket("127.0.0.1",3000);
+ BufferedReader rs= new BufferedReader(new InputStreamReader(myClient.getInputStream()));
+ String str=rs.readLine();
+ System.out.println(str);
+ BufferedReader scan=new BufferedReader(new InputStreamReader(System.in));
+ str=scan.readLine();
+ //System.out.println(str);
+ PrintWriter out=new PrintWriter(myClient.getOutputStream(),true);
+ out.println(str);
+BufferedReader inp1= new BufferedReader(new InputStreamReader(myClient.getInputStream()));
+str=inp1.readLine();
+System.out.println(str);
+ 
+ }
+ catch(Exception e)
+{
+ System.out.println(e);
+}
+ }
+ 
+}
+
